8b63f8361169a5fe0f9dd3502141908aba6f7052
madhukar4080 <madhukar4080@gmail.com>
2026-01-25 20:58:21 -0600

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>🚀 React Frontend Successfully Deployed!</h1>
+      <img src="/deployed.png" alt="Deployment Successful" />
+      <p>Azure DevOps CI/CD Pipeline worked perfectly.</p>
+    </div>
+  );
+}
+
+export default App;
